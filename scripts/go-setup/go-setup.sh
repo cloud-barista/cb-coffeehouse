6ec46b0995a26e08f109ae3d53dc20eebe8a1232
Yunkon (Alvin) Kim <hermitkim1@users.noreply.github.com>
2020-08-21 13:27:20 +0900

    Create go-setup.sh

---
+++ scripts/go-setup/go-setup.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+echo "Go setup script"
+sleep 3
+
+echo ""
+echo "1. Change direcotry to your home directory"
+sleep 3
+cd $HOME
+
+echo""
+echo -n "Current directory: "
+echo $PWD
+
+echo ""
+echo "2. Package update"
+sleep 3
+sudo apt update -y
+
+echo ""
+echo "3. Download Go1.12.7"
+sleep 3
+wget https://dl.google.com/go/go1.12.7.linux-amd64.tar.gz
+
+echo ""
+echo "4. Decompress the downloaded file"
+sleep 3
+tar -xvf go1.12.7.linux-amd64.tar.gz
+
+echo ""
+echo "5. Create gosrc directory"
+sleep 3
+mkdir $HOME/gosrc
+echo -n "Current directory: "
+echo $PWD
+
+echo ""
+echo "6. Set environment path"
+sleep 3
+echo "export GOPATH=$HOME/gosrc;" >> .bashrc
+echo "export GOROOT=$HOME/go;" >> .bashrc
+echo "export PATH=\$PATH:\$GOROOT/bin;" >> .bashrc
+
+echo ""
+echo "7. Apply the environment path"
+sleep 3
+. $HOME/.bashrc
+
+
+echo ""
+echo "8. Check Go version"
+sleep 3
+go version
