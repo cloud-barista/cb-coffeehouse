0554bbedd3e84134c4e7d63ea8a527c076946314
Yunkon (Alvin) Kim <hermitkim1@users.noreply.github.com>
2020-11-11 17:48:23 +0900

    Update go1.15.3-setup.sh

--- scripts/go-setup/go1.15.3-setup.sh
+++ scripts/go-setup/go1.15.3-setup.sh
@@ -18,24 +18,29 @@ sleep 3
 sudo apt update -y
 
 echo ""
-echo "3. Download Go1.15.3"
+echo "3. Package upgrade"
+sleep 3
+sudo apt dist-upgrade -y
+
+echo ""
+echo "4. Download Go1.15.3"
 sleep 3
 wget https://dl.google.com/go/go1.15.3.linux-amd64.tar.gz
 
 echo ""
-echo "4. Decompress the downloaded file"
+echo "5. Decompress the downloaded file"
 sleep 3
 tar -xvf go1.15.3.linux-amd64.tar.gz
 
 echo ""
-echo "5. Create gosrc directory"
+echo "6. Create gosrc directory"
 sleep 3
 mkdir $HOME/gosrc
 echo -n "Current directory: "
 echo $PWD
 
 echo ""
-echo "6. Set environment path"
+echo "7. Set environment path"
 sleep 3
 echo "" >> .bashrc
 echo "export GOPATH=\$HOME/gosrc;" >> .bashrc
@@ -43,12 +48,12 @@ echo "export GOROOT=\$HOME/go;" >> .bashrc
 echo "export PATH=\$PATH:\$GOROOT/bin;" >> .bashrc
 
 echo ""
-echo "7. Apply the environment path"
+echo "8. Apply the environment path"
 sleep 3
 . $HOME/.bashrc
 
 
 echo ""
-echo "8. Check Go version"
+echo "9. Check Go version"
 sleep 3
 go version
